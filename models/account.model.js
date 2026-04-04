23bcb58ffeb5f30326a5d3dd2ff09b1a7c9d6f81
Ritesh Maurya <riteshkumar411552@gmail.com>
2026-04-04 09:52:35 +0530

    add account model with schema for user, status, and currency

---
+++ models/account.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+
+const accountSchema = new mongoose.Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:[true, "Account must be associate with a user"]
+    },
+    status:{
+        enum:{
+            values:["ACTIVE", "FROZEN", "CLOSED"],
+            message:"Status can be either ACTIVE, FROZEN or CLOSED"
+        }
+    },
+    currency:{
+        type:String,
+        required:[true, "Currency is required for creating an account"],
+        default:"INR"
+    }
+
+},{timestamps:true})
+
+export const Account = mongoose.model("Account", accountSchema)
