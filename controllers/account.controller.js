1d0257fab05c60d3d987816ca4d9e6c5de8b6627
Ritesh Maurya <riteshkumar411552@gmail.com>
2026-04-04 10:25:32 +0530

    fix: correct ApiError to ApiResponse in createAccount function and adjust account model schema formatting

--- controllers/account.controller.js
+++ controllers/account.controller.js
@@ -7,7 +7,7 @@ export const createAccount = async(req, res, next)=> {
     try {
         const user = req.user;
         const account = await Account.create({user:user._id})
-        return res.status(201).json(new ApiError(true, {account}, "Account created successfully"))
+        return res.status(201).json(new ApiResponse(true, {account}, "Account created successfully"))
     } catch (error) {
         console.error(error)
         next(error)
